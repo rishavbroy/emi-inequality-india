238c5dcf8518e6925d0a53609b1d7fc0ba1a2d7f
Rishav Roy <roybrishav@gmail.com>
2026-07-22 20:53:27 -0500

    refactor(districts): centralize Census 2001 state names

--- R/districts/build_district_keys.R
+++ R/districts/build_district_keys.R
@@ -29,6 +29,50 @@ canonicalize_state_name <- function(x) {
   out
 }
 
+#' Census 2001 state name from official location code
+#'
+census_2001_state_name <- function(code) {
+  key <- sprintf("%02d", as.integer(num(code)))
+  lookup <- c(
+    "01" = "Jammu & Kashmir",
+    "02" = "Himachal Pradesh",
+    "03" = "Punjab",
+    "04" = "Chandigarh",
+    "05" = "Uttaranchal",
+    "06" = "Haryana",
+    "07" = "Delhi",
+    "08" = "Rajasthan",
+    "09" = "Uttar Pradesh",
+    "10" = "Bihar",
+    "11" = "Sikkim",
+    "12" = "Arunachal Pradesh",
+    "13" = "Nagaland",
+    "14" = "Manipur",
+    "15" = "Mizoram",
+    "16" = "Tripura",
+    "17" = "Meghalaya",
+    "18" = "Assam",
+    "19" = "West Bengal",
+    "20" = "Jharkhand",
+    "21" = "Orissa",
+    "22" = "Chhattisgarh",
+    "23" = "Madhya Pradesh",
+    "24" = "Gujarat",
+    "25" = "Daman & Diu",
+    "26" = "Dadra & Nagar Haveli",
+    "27" = "Maharashtra",
+    "28" = "Andhra Pradesh",
+    "29" = "Karnataka",
+    "30" = "Goa",
+    "31" = "Lakshadweep",
+    "32" = "Kerala",
+    "33" = "Tamil Nadu",
+    "34" = "Pondicherry",
+    "35" = "Andaman & Nicobar Islands"
+  )
+  unname(lookup[key])
+}
+
 #' make district key
 #'
 make_district_key <- function(state, district, year) {
